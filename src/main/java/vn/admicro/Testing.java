140c29ff0e884eeb6eb0190d86728f484971a50e
lvhuong221 <levanhuong221@gmail.com>
2018-08-21 15:09:03 +0700

    wite parquet function

--- src/main/java/vn/admicro/Testing.java
+++ src/main/java/vn/admicro/Testing.java
@@ -2,22 +2,38 @@ package vn.admicro;
 
 
 import com.google.gson.Gson;
+import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.MessageTypeParser;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class Testing {
+    private static final MessageType schema = MessageTypeParser.parseMessageType(
+            "Message DataModel{\n" +
+                    "required int32 timeCreate(DATE);\n" +
+                    "required int32 cookieCreate(DATE);\n" +
+                    "required int32 browserCode;\n" +
+                    "required binary browserVer (UTF8);\n" +
+                    "required int32 osCode;\n" +
+                    "required binary osVer (UTF8);\n" +
+                    "required int64 ip;\n" +
+                    "required int32 locId;\n" +
+                    "required binary domain(UTF8);\n" +
+                    "required int32 siteId;\n" +
+                    "required int32 cId;\n" +
+                    "required binary path (UTF8);\n" +
+                    "required binary referer (UTF8);\n" +
+                    "required int64 guid;\n" +
+                    "required binary flashVersion (UTF8);\n" +
+                    "required binary jre (UTF8);\n" +
+                    "required binary sr (UTF8);\n" +
+                    "required binary sc (UTF8);\n" +
+                    "required int32 geopraphic;}");
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Convert text file to Parquet");
-        SparkContext sc = new SparkContext(conf);
-        SparkSession session = new SparkSession(sc);
-
-        DataModel model = new DataModel();
-        Gson gson = new Gson();
-        String json = gson.toJson(model);
-        // Create the DataFrame
-        Dataset df = session.read().json(json);
 
     }
 }
--- src/main/java/vn/admicro/WriteAsParquet.java
+++ src/main/java/vn/admicro/WriteAsParquet.java
@@ -1,60 +1,41 @@
 package vn.admicro;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.parquet.column.ParquetProperties;
-import org.apache.parquet.example.data.Group;
-import org.apache.parquet.example.data.GroupFactory;
-import org.apache.parquet.example.data.simple.SimpleGroupFactory;
-import org.apache.parquet.hadoop.ParquetWriter;
-import org.apache.parquet.hadoop.example.GroupWriteSupport;
-import org.apache.parquet.schema.MessageType;
-import org.apache.parquet.schema.MessageTypeParser;
 import org.apache.spark.SparkConf;
-import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
 
 import java.io.IOException;
 
 public class WriteAsParquet {
+
     public static void main(String[] args) throws IOException {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL basic example")
+                .config("spark.some.config.option", "some-value")
+                .getOrCreate();
         SparkConf conf = new SparkConf().setAppName("Convert text file to Parquet");
-        SparkContext sc = new SparkContext(conf);
-        Path inputPath = new Path(args[0]);
-        MessageType schema = MessageTypeParser.parseMessageType(
-                "Message DataModel{\n" +
-                        "required int32 timeCreate(DATE);\n" +
-                        "required int32 cookieCreate(DATE);\n" +
-                        "required int32 browserCode;\n" +
-                        "required binary browserVer (UTF8);\n" +
-                        "required int32 osCode;\n" +
-                        "required binary osVer (UTF8);\n" +
-                        "required int64 ip;\n" +
-                        "required int32 locId;\n" +
-                        "required binary domain(UTF8);\n" +
-                        "required int32 siteId;\n" +
-                        "required int32 cId;\n" +
-                        "required binary path (UTF8);\n" +
-                        "required binary referer (UTF8);\n" +
-                        "required int64 guid;\n" +
-                        "required binary flashVersion (UTF8);\n" +
-                        "required binary jre (UTF8);\n" +
-                        "required binary sr (UTF8);\n" +
-                        "required binary sc (UTF8);\n" +
-                        "required int32 geopraphic;}");
-        Configuration confH = new Configuration();
-        GroupWriteSupport writeSupport = new GroupWriteSupport();
-        GroupWriteSupport.setSchema(schema, confH);
-        GroupFactory groupFactory = new SimpleGroupFactory(schema);
-        Group group = groupFactory.newGroup();
-        ParquetWriter<Group> writer = new ParquetWriter<Group>(inputPath, writeSupport,
-                ParquetWriter.DEFAULT_COMPRESSION_CODEC_NAME,
-                ParquetWriter.DEFAULT_BLOCK_SIZE,
-                ParquetWriter.DEFAULT_PAGE_SIZE,
-                ParquetWriter.DEFAULT_PAGE_SIZE, /* dictionary page size */
-                ParquetWriter.DEFAULT_IS_DICTIONARY_ENABLED,
-                ParquetWriter.DEFAULT_IS_VALIDATING_ENABLED,
-                ParquetProperties.WriterVersion.PARQUET_1_0, confH);
-        writer.write(group);
-        writer.close();
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> input = sc.textFile(args[0]);
+        JavaRDD<Row> output = input.map(new Function<String, Row>() {
+            @Override//Trả về Row từ String tròngile
+            public Row call(String s) throws Exception {
+                String[] split = s.split("\t");
+                Row output = RowFactory.create(split[0-18]);
+                return output;
+            }
+        });
+        RDD<Row> rdd = output.rdd();
+        Dataset<Row> df = spark.createDataFrame(rdd, DataModel.class);
+        df.printSchema();
+        df.show(20);
+        df.write().parquet(args[1]);
     }
 }
