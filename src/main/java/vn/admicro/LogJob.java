e502fe28d8837e59a713e93c071f381ed678b8a7
lvhuong221 <levanhuong221@gmail.com>
2019-03-17 15:25:35 +0700

    finish job1 and job2, and some comment

--- src/main/java/vn/admicro/LogJob.java
+++ src/main/java/vn/admicro/LogJob.java
@@ -1,20 +1,15 @@
 package vn.admicro;
 
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.ReduceFunction;
-import org.apache.spark.sql.Column;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.codehaus.janino.Java;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import scala.Tuple2;
 
-import static org.apache.hadoop.yarn.webapp.hamlet.HamletSpec.Scope.col;
-import static org.apache.spark.sql.functions.*;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.desc;
 
 
 public class LogJob {
@@ -29,7 +24,7 @@ public class LogJob {
                 .getOrCreate();
         spark.conf().set("spark.executor.memory", "1G");
 
-        switch (args[1]){
+        switch (args[2]){
             case "1":   job1(spark,args);   break;
             case "2":   job2(spark,args);   break;
             case "3":   job3(spark,args);  break;
@@ -37,27 +32,35 @@ public class LogJob {
         }
     }
     public static void job1(SparkSession spark, String[] args){
-        //url được truy cập nhiều nhất, chưa xét guid
+        //url được truy cập nhiều nhất trong ngày của mỗi guid
+        //Find the most accessed url of the day of each guid?
         Dataset<Row> data = spark.read().parquet(args[0])
                 .select(col("referer"), col("guid"));
-        Dataset<Row> df = data.groupBy(col("referer")).count().sort(desc("count")).limit(1000);
-        df.coalesce(1).toJavaRDD().saveAsTextFile(args[2]);
+        Dataset<Row> df = data.distinct().groupBy(col("referer"))
+                .count().sort(desc("count"));
+        /*
+        Dataset<Row> df = data.groupBy(col("referer")).
+                count().sort(desc("count"));*/
+
+        df.coalesce(1).toJavaRDD().saveAsTextFile(args[1]);
         spark.close();
     }
+    //Tìm các IP được sử dụng bởi nhiều guid nhất,
+    //guid không lặp lại, không tính số lần sử dụng, chỉ tính số guid
     public static void job2(SparkSession spark, String[] args){
         Dataset<Row> data = spark.read().parquet(args[0])
-                .select(col("guid"), col("domain"));
-        String count1 = data.dropDuplicates("path").count()+"";
-        String count2 = data.dropDuplicates("domain").count()+"";
-        System.out.println("path doc nhat: "+count1);
-        System.out.println("domain doc nhat: "+count2);
+                .select(col("guid"), col("ip"));
+        Dataset<Row> df = data.distinct().groupBy(col("ip")).count().
+                sort(desc("count"));
+        df.coalesce(1).toJavaRDD().saveAsTextFile(args[1]);
+        spark.close();
     }
     public static void job3(SparkSession spark, String[] args){
-        //Tính các guid mà có timeCreate – cookieCreate nhỏ hơn 30 phút
+        //Tìm các guid mà có timeCreate – cookieCreate nhỏ hơn 30 phút
         String[] column = {"timeCreate","cookieCreate","guid"};
         Dataset<Row> data = spark.read().parquet(args[0])
                 .select(col("timeCreate"), col("cookieCreate"));
-        data = data.filter(row -> {
+        data = data.filter((FilterFunction<Row>) row -> {
             String timeCreate = row.getString(0)+"";
             String cookieCreate = row.get(1)+"";
             DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
@@ -70,10 +73,13 @@ public class LogJob {
              *  Neu timeCreate < cookieCreate + 30' -> true
              */
         });
+        /*
         data.show();
         System.out.println("Số guid thỏa mãn"+data.count());
         data = data.dropDuplicates("guid");
-        System.out.println("So guid doc nhat:"+data.count());
-
+        System.out.println("So guid độc nhat:"+data.count());
+        */
+        data.coalesce(1).toJavaRDD().saveAsTextFile(args[1]);
+        spark.close();
     }
 }
--- src/main/java/vn/admicro/WriteAsParquet.java
+++ src/main/java/vn/admicro/WriteAsParquet.java
@@ -7,10 +7,10 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import scala.Function1;
 
 import java.io.IOException;
 public class WriteAsParquet {
+    //Full log structure
     private static final StructField[] fields = new StructField[]{
             new StructField("timeCreate", DataTypes.StringType, false, Metadata.empty()),
             new StructField("cookieCreate", DataTypes.StringType, false,Metadata.empty()),
@@ -46,20 +46,13 @@ public class WriteAsParquet {
         WriteParquet(spark, args);
     }
     public static void WriteParquet(SparkSession spark, String[] args ){
+        //Get rid of unnecessary column
         String[] dropList = {"c19","c20","c21","c22","c23"};
-        Dataset<Row> data = spark.read().format("csv").option("delimiter", "\t").schema(schema).load(args[0]);
+        Dataset<Row> data =
+                spark.read().format("csv")
+                        .option("delimiter", "\t")
+                        .schema(schema).load(args[0]);
         data = data.drop(dropList);
-        /*
-        data.toJavaRDD().map(row -> {
-            String str = row.get(0)+"";
-            str.replaceAll("-",":");
-            return row;
-        }).collect();
-        */
-        /*
-        data.printSchema();
-        data.show();
-        */
         data.coalesce(1).write().format("parquet")
                 .parquet(args[1]);
         spark.close();
